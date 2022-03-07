227804009aaef9c5cc62ed92209a6e6cea6ec46e
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2022-03-07 15:55:54 -0800

    Add files via upload

--- HRV Monitor/HRV Monitor WatchKit Extension/Views/NotificationView.swift
+++ HRV Monitor/HRV Monitor WatchKit Extension/Views/NotificationView.swift
@@ -8,10 +8,20 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @EnvironmentObject var workoutManager: WorkoutManager
+
     var body: some View {
-        Text("Hello, World!")
+    VStack {
+        Text("Notification").font(.title2).padding(.bottom, 10)
+               Button {
+                    workoutManager.resume()
+                } label: {
+                    Image(systemName: "return.right").font(.title).padding(.top, 3).padding(.bottom, 3);
+                }.foregroundColor(.white)
+            }
+            .padding()
+          }
     }
-}
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
