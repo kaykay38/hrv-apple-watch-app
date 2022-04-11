8327208b4536b58f745b85b503d49dd19cd1fb4c
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2022-04-11 10:55:22 -0700

    add suvery.swift
    This may or may not stay at the end of collaboration

---
+++ HRV Monitor/HRV Monitor WatchKit Extension/Views/SurveyView.swift
@@ -0,0 +1,21 @@
+//
+//  SurveyView.swift
+//  HRV Monitor WatchKit Extension
+//
+//  Created by Whitney Bolar on 4/11/22.
+//
+
+// It is possible we may want the survey on a different folder
+import SwiftUI
+
+struct SurveyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SurveyView_Previews: PreviewProvider {
+    static var previews: some View {
+        SurveyView()
+    }
+}
