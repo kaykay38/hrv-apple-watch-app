95e8703a5d3e7d4938a37c9d62a373f76cef03f5
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2022-05-01 21:02:57 -0700

    retunred to root but confirmation unsuccessful

--- HRV Monitor/HRV Monitor WatchKit Extension/Views/NotificationView.swift
+++ HRV Monitor/HRV Monitor WatchKit Extension/Views/NotificationView.swift
@@ -75,17 +75,29 @@ struct NotificationView: View {
          NotificationManager.instance.scheduleHighNotification()
         }
         
-        Button("Notification Page: got to survey") {
-            self.modalState.isModal1Presented = true
-            
-        }.sheet(isPresented: $modalState.isModal1Presented) {
-            ModalView(modalState: self.modalState)
-        }
+        VStack {
+            HStack{
+                Image(systemName: "exclamationmark.square").font(.title).padding(.bottom, 3).foregroundColor(.yellow).padding(.bottom, 5);
+             
+                Text("Episode Detected").font(.title3).padding(.bottom, 10)
+            }
+        
+            Button {
+                self.modalState.isModal1Presented = true
+            } label: {
+                Text("False Alarm")
+                    .font(.title3)
+                    .foregroundColor(.green);
+            }.padding(.top, 10)
+                .sheet(isPresented: $modalState.isModal1Presented) {
+                    ModalView(modalState: self.modalState)
+                }
         
         Button("Dismiss") {
             NotificationManager.instance.activeAlert = false
-        }.onAppear(perform: hapticController.triggerHaptic)
+        }//.onAppear(perform: hapticController.triggerHaptic)
     }
+ }
 }
 // modal 1
 struct ModalView: View {
@@ -93,36 +105,90 @@ struct ModalView: View {
     @ObservedObject var notificationManager:NotificationManager = NotificationManager.instance
     var body: some View {
         VStack {
-            Text("Inside Modal View")
-                .padding()
-            Button("Survey Page: got to Confirmation") {
-                self.modalState.isModal2Presented = true
-            }.sheet(isPresented: $modalState.isModal2Presented) {
-                Modal2(modalState: self.modalState)
-            }
-            Button("Dismiss") {
+            ScrollView{
+                Text("What's your stress level?").font(.title3).padding(.top, 5)
+                
+                Button {
+                    self.modalState.isModal1Presented = false
+                    NotificationManager.instance.activeAlert = false
+                    //self.modalState.isModal2Presented = true
+                } label: {
+                    Text("Low")
+                        .font(.title3)
+                        .foregroundColor(.green);
+                }.padding(.top, 10)
+                    /*.sheet(isPresented: $modalState.isModal2Presented) {
+                        Modal2(modalState: self.modalState)
+                    }*/
+                
+                Button {
+                    self.modalState.isModal1Presented = false
+                    NotificationManager.instance.activeAlert = false
+                    //self.modalState.isModal2Presented = true
+                } label: {
+                    Text("Moderate")
+                        .font(.title3)
+                        .foregroundColor(.yellow);
+                }.padding(.top, 10)
+                    /*.sheet(isPresented: $modalState.isModal2Presented) {
+                        Modal2(modalState: self.modalState)
+                    }*/
+                
+                Button {
+                    self.modalState.isModal1Presented = false
+                    NotificationManager.instance.activeAlert = false
+                    //self.modalState.isModal2Presented = true
+                } label: {
+                    Text("High")
+                        .font(.title3)
+                        .foregroundColor(.red);
+                }.padding(.top, 10)
+                    /*.sheet(isPresented: $modalState.isModal2Presented) {
+                        Modal2(modalState: self.modalState)
+                    }*/
+                
+                Button {
+                    self.modalState.isModal1Presented = false
+                    NotificationManager.instance.activeAlert = false
+                    //self.modalState.isModal2Presented = true
+                } label: {
+                    Text("Very High")
+                        .font(.title3)
+                        .foregroundColor(.purple);
+                }.padding(.top, 10)
+                    /*.sheet(isPresented: $modalState.isModal2Presented) {
+                        Modal2(modalState: self.modalState)
+                    }*/
+                
+           /* Button("Dismiss") {
                 self.modalState.isModal1Presented = false
                 NotificationManager.instance.activeAlert = false
-            }
+            }*/
+          }
         }
     }
 }
 // modal 2
-struct Modal2: View {
+/*struct Modal2: View {
     @ObservedObject var modalState: ModalState
     @ObservedObject var notificationManager:NotificationManager = NotificationManager.instance
     var body: some View {
         VStack {
-            Text("Confirmation Page: go to root")
-                .padding()
-            Button("Dismiss") {
-                NotificationManager.instance.activeAlert = false
+            Text("Are you sure?").font(.title3).padding(.top, 10)
+            
+            Button {
                 self.modalState.isModal1Presented = false
-                //self.modalState.isModal2Presented = false
-            }
+                self.modalState.isModal2Presented = false
+                NotificationManager.instance.activeAlert = false
+            } label: {
+                Text("Go Back")
+                    .font(.subheadline)
+            }.foregroundColor(.white)
+            .padding()
+            
         }
     }
-}
+}*/
     
 
 struct NotificationView_Previews: PreviewProvider {
