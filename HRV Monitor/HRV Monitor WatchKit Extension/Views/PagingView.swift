b83a1a6e143ada2e635c98fe2576ff4bc5827f3e
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2022-05-02 20:00:52 -0700

    Setting up separate survey view
    I have modals in with the notification view. I am going to see if I can connect the observable object to the view and or the modal so the data goes through the same place

--- HRV Monitor/HRV Monitor WatchKit Extension/Views/PagingView.swift
+++ HRV Monitor/HRV Monitor WatchKit Extension/Views/PagingView.swift
@@ -22,6 +22,7 @@ struct PagingView: View {
     var body: some View {
         TabView(selection: $selection) {
             //NotificationView(modalState: ModalState())
+            SurveyView()
             ControlsView().tag(Tab.controls)
             ChartView().tag(Tab.liveHRV)
 //            AreaChartView().tag(Tab.liveHRV)
